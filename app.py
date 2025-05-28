36b4cfcddf9bf37d6f1426457d3386ad8115e9fe
MontserratJB <mjimenez@munimoca.local>
2025-05-28 10:11:30 -0600

    actualizacion app

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import dash
 from dash import html
 
+# Crear la app Dash
 app = dash.Dash(__name__)
+
+# Layout simple para prueba
 app.layout = html.Div("Hola desde Dash")
 
+# Este bloque permite correr localmente
 if __name__ == "__main__":
     app.run_server(debug=True)
